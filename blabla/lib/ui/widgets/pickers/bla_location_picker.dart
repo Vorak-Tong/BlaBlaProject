1602448f417acf268251440ce7090f2e6c10ce1c
Vorak Tong <viraktong197@gmail.com>
2026-03-19 09:41:53 +0700

    BLA-202 - Location Picker

--- blabla/lib/ui/widgets/pickers/bla_location_picker.dart
+++ blabla/lib/ui/widgets/pickers/bla_location_picker.dart
@@ -1,6 +1,7 @@
-import 'package:blabla/services/location_service.dart';
+import 'package:blabla/data/repositories/location/location_repository.dart';
 import 'package:blabla/ui/widgets/display/bla_divider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../model/ride/locations.dart';
 import '../../theme/theme.dart';
@@ -47,10 +48,12 @@ class _BlaLocationPickerState extends State<BlaLocationPicker> {
   }
 
   List<Location> get filteredLocation {
+    final locationRepository = context.read<LocationRepository>();
+    final locations = locationRepository.getAvailableLocations();
     if (currentSearchText.length < 2) {
       return [];
     }
-    return LocationsService.availableLocations
+    return locations
         .where(
           (location) => location.name.toUpperCase().contains(
             currentSearchText.toUpperCase(),
